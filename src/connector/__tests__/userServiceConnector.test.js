922077403fbc959f9cee7e27383ded8edc99769e
alibombaye <alibombaye@gmail.com>
2018-10-15 20:13:35 +0100

    dummy data for users

--- src/connector/__tests__/userServiceConnector.test.js
+++ src/connector/__tests__/userServiceConnector.test.js
@@ -7,7 +7,13 @@ describe('userServiceConnector', () => {
             test('responds with a UserNotRecognisedError', async () => {
                 const NOT_A_VALID_USER = 999;
                 expect(() => findUser(NOT_A_VALID_USER)).toThrowError(new UserNotRecognisedError('not a recognised user'));
-            })
+            });
+        });
+
+        describe('when the user does exist', () => {
+            test('responds with true if the user does exist', () => {
+                expect(findUser(1)).toBe(true);
+            });
         })
     });
 })
--- src/connector/userServiceConnector.js
+++ src/connector/userServiceConnector.js
@@ -1,5 +1,23 @@
 import { UserNotRecognisedError } from "../errors/errors";
 
+const users = {
+    1: {
+        name: 'Thanos'
+    },
+    2: {
+        name: 'Iron man'
+    },
+    3: {
+        name: 'Spiderman'
+    }
+};
+
+
 export const findUser = userId => {
+    const user = users[userId];
+
+    if (user) {
+        return true;
+    }
     throw new UserNotRecognisedError('not a recognised user');
 }
