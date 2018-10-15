6638e776ef3b72e149c5013dd642f6677741a6ab
alibombaye <alibombaye@gmail.com>
2018-10-15 15:00:23 +0100

    added test to add a different user to db

--- src/service/__tests__/dbService.test.js
+++ src/service/__tests__/dbService.test.js
@@ -4,11 +4,19 @@ describe('dbService', () => {
     describe('dbAddStreamToUserWatchingList', () => {
         describe('when the user is not watching anything', () => {
             test('returns true and adds the user to the watching list with the stream', () => {
-                expect(watchingDb[1]).toEqual(undefined);
+                watchingDb = {};
                 
                 expect(dbAddStreamToUserWatchingList(1,1)).toBe(true);
                 
                 expect(watchingDb).toEqual({ 1: { streams: [1]} });
+            });
+
+            test('adds a different user', () => {
+                watchingDb = { 1: { streams: [1] }};
+
+                expect(dbAddStreamToUserWatchingList(2,1)).toBe(true);
+
+                expect(watchingDb).toEqual({ 1: { streams: [1]}, 2: { streams: [1] } });
             });
         });
 
